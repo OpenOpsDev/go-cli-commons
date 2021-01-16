ecec4bf8e314eb823600d2ca94907a3b9cc906d1
Roger King <rogr.king@gmail.com>
2021-01-16 16:35:46 -0500

    feat: userinput defaults

--- prompts/input.go
+++ prompts/input.go
@@ -3,12 +3,17 @@ package prompts
 import "github.com/manifoldco/promptui"
 
 type UserInput struct {
-	Label string
+	Label   string
+	Default string
 }
 
 func (u *UserInput) Run() (string, error) {
 	prompt := promptui.Prompt{
-		Label:    u.Label,
+		Label: u.Label,
+	}
+
+	if len(u.Default) > 0 {
+		prompt.Default = u.Default
 	}
 
 	return prompt.Run()
